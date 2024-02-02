98ca573bf9d11725adc040503b8b5e271fa4745d
Sumit Kumar <sumitkumar@jmangroup.com>
2024-02-02 18:08:30 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Timesheet from './pages/Timesheet';
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import HeadlessDemo from './components/Sidebar';
+
+import { Button } from 'primereact/button';
+import Hello from './components/Sidebar';
+        
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+    <HeadlessDemo />
+    <Button label="Submit" />
+    <Timesheet />
+    <Hello />
+    
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+        
+import 'primeicons/primeicons.css';
+// import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';  
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <PrimeReactProvider value={{ ripple: true }}>
+
     <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 );
 
